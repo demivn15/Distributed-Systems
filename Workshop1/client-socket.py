708de0fba3a85defbdc209d7a2e39922bb5e1f74
demivn15 <demivn15@gmail.com>
2026-08-20 11:11:05 -0500

    Multiple clients handling for client-socket.py

--- Workshop1/client-socket.py
+++ Workshop1/client-socket.py
@@ -1,36 +1,60 @@
 # Client socket in Python
 
 from socket import *
+import threading
 
-serverName = input("Enter server hostname or IP address: ")
-if not serverName:
-    serverName = "localhost"
-try:
-    serverPort = int(input("Enter server port number: "))
-except:
-    print("Invalid input. Using default port 12000.")
-    serverPort = 12000
+SERVER_NAME:str = "localhost"
+DEFAULT_PORT:int = 12000
+MIN_PORT:int = 0
+MAX_PORT:int = 65535
 
-if serverPort <= 0 or serverPort > 65535:
-    serverPort = 12000
 
-next = True
-while next:
-    clientSocket = socket(AF_INET, SOCK_STREAM)
+def connectServer() -> tuple:
+    serverName = input("Enter server hostname or IP address: ")
+    if not serverName:
+        serverName = SERVER_NAME
     try:
-        clientSocket.connect((serverName, serverPort))
-    except Exception as e:
-        print("Connection error:", e)
-        repeat = input("Do you want to try again? (Y/N)")
-        if repeat.upper() == "N":
-            break
-        else:
-            continue
-    sentence = input("Input lowercase sentence:")
-    clientSocket.send(sentence.encode())
-    modifiedSentence = clientSocket.recv(1024)
-    print("From Server:", modifiedSentence.decode())
-    other = input("Other message: (Y/N)")
-    if other.upper() == "N":
-        next = False
-    clientSocket.close()
+        serverPort = int(input("Enter server port number: "))
+    except:
+        print("Invalid input. Using default port", DEFAULT_PORT)
+        serverPort = DEFAULT_PORT
+
+    if serverPort <= MIN_PORT or serverPort > MAX_PORT:
+        serverPort = DEFAULT_PORT
+    return (serverName, serverPort)
+
+def sendMessage(serverName:str, serverPort:int) -> None:
+    next:bool = True
+    while next:
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        try:
+            clientSocket.connect((serverName, serverPort))
+        except Exception as e:
+            print("Connection error:", e)
+            repeat = input("Do you want to try again? (Y/N)")
+            if repeat.upper() == "N":
+                break
+            else:
+                continue
+        sentence = input("Input lowercase sentence:")
+        clientSocket.send(sentence.encode())
+        modifiedSentence = clientSocket.recv(1024)
+        print("From Server:", modifiedSentence.decode())
+        other = input("Other message: (Y/N)")
+        if other.upper() == "N":
+            next = False
+        clientSocket.close()
+
+def client() -> None:
+    serverInfo  = connectServer()
+    sendMessage(serverInfo[0], serverInfo[1])
+
+def main() -> None:
+    thread1 = threading.Thread(target = client)
+    thread2 = threading.Thread(target = client)
+
+    thread1.start()
+    thread1.join()
+
+if __name__ == "__main__":
+    main()
