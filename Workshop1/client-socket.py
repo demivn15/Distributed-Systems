6997754ea394201a018328d56936fee47781c9b1
demivn15 <demivn15@gmail.com>
2026-08-20 16:19:15 -0500

    Changes to c files.

--- Workshop1/client-socket.py
+++ Workshop1/client-socket.py
@@ -1,67 +1,67 @@
 # Client socket in Python
 
+import logging
 from socket import *
 import threading
-import logging
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
+    level = logging.INFO,
+    format = "%(asctime)s - %(levelname)s - %(message)s"
 )
 
-DEFAULT_NUMBER_CLIENTS:int = 3
+DEFAULT_NUMBER_OF_CLIENTS:int = 3
 DEFAULT_SERVER_NAME:str = "172.23.202.123"
 DEFAULT_PORT_NUMBER:int = 12000
 MIN_PORT_NUMBER:int = 0
 MAX_PORT_NUMBER:int = 65535
-DATA_CHUNK:int = 1024
+BYTES_DATA_CHUNK:int = 1024
 
-def serverIdentification() -> tuple:
-    serverName = input("Enter server hostname or IP address: ")
+def serverIdentification(clientID:int) -> tuple:
+    serverName:str = input("Enter server hostname or IP address: ")
     if not serverName:
         serverName = DEFAULT_SERVER_NAME
-    try:
-        serverPort = int(input("Enter server port number: "))
-    except:
-        logging.info(f"Invalid input. Using default port {DEFAULT_PORT_NUMBER}")
+        logging.info(f"No IP address specified. Using default address: {DEFAULT_SERVER_NAME}")
+    serverPort:str = input("Enter server port number: ")
+    if not serverPort:
         serverPort = DEFAULT_PORT_NUMBER
+        logging.info(f"No port number specified. Using default port number: {DEFAULT_PORT_NUMBER}")
     if serverPort <= MIN_PORT_NUMBER or serverPort > MAX_PORT_NUMBER:
         serverPort = DEFAULT_PORT_NUMBER
+    logging.info(f"Client {clientID} trying to connect to the server...")
     return (serverName, serverPort)
 
-def client(clientID:int, serverName:str, serverPort:int ) -> None:
+def clientInitialization(clientID:int, serverName:str, serverPort:int) -> None:
     next:bool = True
     while next:
-        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket:socket.socket = socket(AF_INET, SOCK_STREAM)
         try:
             clientSocket.connect((serverName, serverPort))
         except Exception as exception:
             logging.info(exception)
-            repeat = input("Do you want to try again? (Y/N)")
+            repeat:str = input("Do you want to try again? (Y/N)")
             if repeat.upper() == "N":
                 break
             else:
                 continue
-        sentence = input(f"Enter a lowercase sentence for client {clientID}:")
+        logging.info(f"Client {clientID} successfully connected to the server.")
+        sentence:str = input(f"Enter a lowercase sentence for client {clientID}:")
         clientSocket.send(sentence.encode())
-        modifiedSentence = clientSocket.recv(DATA_CHUNK)
+        modifiedSentence:str = clientSocket.recv(BYTES_DATA_CHUNK)
         print("Message from server:", modifiedSentence.decode())
-        other = input("Other message: (Y/N)")
+        other:str = input("Other message: (Y/N)")
         if other.upper() == "N":
             next = False
         clientSocket.close()
 
 def main() -> None:
-    clientsNumber:int = input("Enter the number of clients: ")
-    if not clientsNumber:
-        clientsNumber = DEFAULT_NUMBER_CLIENTS
+    numberOfClients:str = input("Enter the number of clients: ")
+    if not numberOfClients:
+        numberOfClients = DEFAULT_NUMBER_OF_CLIENTS
+        logging.info(f"No number of clients specified. Using {DEFAULT_NUMBER_OF_CLIENTS} clients.")
     threads:list = []
-    for _id in range(clientsNumber):
-        serverName, serverPort = serverIdentification()
-        thread = threading.Thread(
-                target = client,
-                args=(_id + 1, serverName, serverPort)
-                )
+    for _id in range(int(numberOfClients)):
+        serverName, serverPort = serverIdentification(_id + 1)
+        thread:threading.Thread = threading.Thread(target = clientInitialization, args = (_id + 1, serverName, serverPort))
         threads.append(thread)
         thread.start()
     for thread in threads:
