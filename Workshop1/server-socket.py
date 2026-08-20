11bd3477dcd502c3dca670c54859e13545a91c0f
demivn15 <demivn15@gmail.com>
2026-08-20 09:38:43 -0500

    Changes to server-socket.py

--- Workshop1/server-socket.py
+++ Workshop1/server-socket.py
@@ -1,10 +1,8 @@
 # Server socket in Python
 
 from socket import *
+import time
 
-serverName = input("Enter server hostname or IP address: ")
-if not serverName:
-    serverName = "localhost"
 try:
     serverPort = int(input("Enter server port number: "))
 except:
@@ -14,23 +12,35 @@ except:
 if serverPort <= 0 or serverPort > 65535:
     serverPort = 12000
 
-next = True
-while next:
-    clientSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(("", serverPort))
+serverSocket.listen(1)
+print("The server is ready to receive")
+while True:
     try:
-        clientSocket.connect((serverName, serverPort))
+        connectionSocket, addr = serverSocket.accept()
+    except KeyboardInterrupt:
+        print("\nServer is shutting down.")
+        break
+    print("From Client:", addr)
+
+    try:
+        sentence = connectionSocket.recv(1024).decode()
     except Exception as e:
-        print("Connection error:", e)
-        repeat = input("Do you want to try again? (Y/N)")
-        if repeat.upper() == "N":
-            break
-        else:
-            continue
-    sentence = input("Input lowercase sentence:")
-    clientSocket.send(sentence.encode())
-    modifiedSentence = clientSocket.recv(1024)
-    print("From Server:", modifiedSentence.decode())
-    other = input("Other message: (Y/N)")
-    if other.upper() == "N":
-        next = False
-    clientSocket.close()
+        print("Error receiving data:", e)
+        connectionSocket.close()
+        continue
+    print("I received:", sentence)
+    capitalizedSentence = sentence.upper()
+    time.sleep(3)
+
+    try:
+        connectionSocket.send(capitalizedSentence.encode())
+    except Exception as e:
+        print("Error sending data:", e)
+
+    try:
+        connectionSocket.close()
+    except Exception as e:
+        print("Error closing connection:", e)
+   clientSocket.close()
