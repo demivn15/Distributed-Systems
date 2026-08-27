048a1312a8323f2909480f545d7ce301cafff5e6
Al ortz <christopher.ortiz@yachaytech.edu.ec>
2026-08-27 10:22:21 -0500

    Actualización de rmi

--- Workshop2/RMI/client-rmi.py
+++ Workshop2/RMI/client-rmi.py
@@ -15,6 +15,27 @@ if serverPort <= 0 or serverPort > 65535:
 
 proxy = xmlrpc.client.ServerProxy(f"http://{serverName}:{serverPort}/RPC2")
 
-# Call the remote method 'add'
-result = proxy.add(8, 3)
-print("8 + 3 =", result)
+matrix1 = np.array([[1, 2], [3, 4]])
+matrix2 = np.array([[5, 6], [7, 8]])
+
+matrix1_list = matrix1.tolist()
+matrix2_list = matrix2.tolist()
+
+
+#Call the remote method 'add, substract, multiply' matrix
+suma_matrix= proxy.add(matrix1_list, matrix2_list)
+resta_matrix= proxy.subtract(matrix1_list, matrix2_list)
+multiplicacion_matrix= proxy.multiply(matrix1_list, matrix2_list)
+
+print("Matrix 1:")
+print(matrix1)
+print("Matrix 2:")
+print(matrix2)
+print("Suma de matrices:")
+print(np.array(suma_matrix))
+print("Resta de matrices:")
+print(np.array(resta_matrix))
+print("Multiplicacion de matrices:")
+print(np.array(multiplicacion_matrix))
+
+
--- Workshop2/RMI/server-rmi.py
+++ Workshop2/RMI/server-rmi.py
@@ -1,5 +1,6 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
+import numpy as np 
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
@@ -23,10 +24,28 @@ with SimpleXMLRPCServer((serverName, serverPort),
     server.register_introspection_functions()
 
     # Register a function under a different name
-    def add(x, y):
-        return x + y
+    def add(matrix1_list, matrix2_list):
 
+        np_matrix1 = np.array(matrix1_list)
+        np_matrix2 = np.array(matrix2_list)
+        return np.add(np_matrix1, np_matrix2).tolist()
+
+    def subtract(matrix1_list, matrix2_list):
+        np_matrix1 = np.array(matrix1_list)
+        np_matrix2 = np.array(matrix2_list)
+        return np.subtract(np_matrix1, np_matrix2).tolist()
+    
+    def multiply(matrix1_list, matrix2_list):
+        np_matrix1 = np.array(matrix1_list)
+        np_matrix2 = np.array(matrix2_list)
+        if np_matrix1.shape[1] != np_matrix2.shape[0]:
+            raise ValueError("Number of columns in the first matrix must be equal to the number of rows in the second matrix for multiplication.")
+        return np.dot(matrix1_list, matrix2_list).tolist()
+
+    
     server.register_function(add, 'add')
+    server.register_function(subtract, 'subtract')
+    server.register_function(multiply, 'multiply')
 
     # Run the server's main loop
     print(f"Server is listening on {serverName}:{serverPort}...")
