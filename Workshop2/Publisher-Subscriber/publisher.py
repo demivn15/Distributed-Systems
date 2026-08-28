4b3088fbdc896f62c0fde8964b30aa26160a0ca5
Al ortz <christopher.ortiz@yachaytech.edu.ec>
2026-08-28 18:05:44 -0500

    Changes

--- Workshop2/Publisher-Subscriber/publisher.py
+++ Workshop2/Publisher-Subscriber/publisher.py
@@ -8,15 +8,7 @@ MAX_SERVER_PORT = 65535
 context = zmq.Context()
 s = context.socket(zmq.PUB)
 
-<<<<<<< HEAD
-serverName = input("Enter server hostname or IP address [default: localhost]: ") or "localhost"
-try:
-    serverPort = int(input("Enter server port number [default: 15000]: ") or 15000)
-except ValueError:
-    serverPort = 15000
 
-p = f"tcp://{serverName}:{serverPort}"
-=======
 serverName = input("Enter server hostname or IP address: ")
 if not serverName:
     serverName = DEFAULT_SERVER_NAME
@@ -30,11 +22,10 @@ if serverPort <= MIN_SERVER_PORT or serverPort > MAX_SERVER_PORT:
     serverPort = DEFAULT_SERVER_PORT
 
 p = "tcp://" + serverName + ":" + str(serverPort)
->>>>>>> d5368acef01f719061c1845f0f2afcc16b53edb2
 s.bind(p)
 
 # Solicitamos el servicio (Topic) que este publicador va a ofrecer
-topic = input("Ingresa el servicio a publicar (ej. FITNESS, NUTRITION): ").upper()
+topic = input("Ingresa el servicio a publicar (ej. FITNESS, NUTRITION): ").strip().upper()
 if not topic:
     topic = "GENERAL"
 
@@ -44,7 +35,6 @@ cont = 0
 while True:
     time.sleep(3) # Pausa de 3 segundos
     cont += 1
-<<<<<<< HEAD
     
     # Generamos un mensaje distinto dependiendo del servicio elegido
     if topic == "FITNESS":
@@ -55,6 +45,3 @@ while True:
         mensaje = f"{topic} - Mensaje genérico #{cont}"
         
     s.send(mensaje.encode("utf-8"))
-=======
-    s.send(("TIME " + time.asctime() + " - Message #" + str(cont)).encode("utf-8"))
->>>>>>> d5368acef01f719061c1845f0f2afcc16b53edb2
--- Workshop2/Publisher-Subscriber/subscriber.py
+++ Workshop2/Publisher-Subscriber/subscriber.py
@@ -8,7 +8,7 @@ MAX_SERVER_PORT = 65535
 context = zmq.Context()
 s = context.socket(zmq.SUB)
 
-<<<<<<< HEAD
+
 serverName = input("Enter server hostname or IP address [default: localhost]: ") or "localhost"
 
 # 1. Nos conectamos a múltiples publicadores (Múltiples llamadas a connect)
@@ -36,26 +36,4 @@ try:
         print(f"Recibido: {mensaje_recibido}")
 except KeyboardInterrupt:
     print("\nSuscriptor desconectado.")
-=======
-serverName = input("Enter server hostname or IP address: ")
-if not serverName:
-    serverName = DEFAULT_SERVER_NAME
-try:
-    serverPort = int(input("Enter server port number: "))
-except:
-    print(f"Invalid input. Using default port {DEFAULT_SERVER_PORT}.")
-    serverPort = DEFAULT_SERVER_PORT
-
-if serverPort <= MIN_SERVER_PORT or serverPort > MAX_SERVER_PORT:
-    serverPort = DEFAULT_SERVER_PORT
-
-p = "tcp://" + serverName + ":" + str(serverPort)
-s.connect(p)
-
-s.setsockopt_string(zmq.SUBSCRIBE, "TIME")
-
-for i in range(5):
-    time = s.recv().decode("utf-8")
-    print(time)
->>>>>>> d5368acef01f719061c1845f0f2afcc16b53edb2
 
